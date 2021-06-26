d90829a9f84a426ffdce6d3846a3ea916341e398
Semrah Ayaz <83428315+semraahayaz@users.noreply.github.com>
2021-06-26 14:46:49 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function Yazdir(){
+    document.getElementById("kutu").innerHTML="Semrah Ayaz </br> 25 ";
+}
+let btnYaz=document.getElementById("btn");
+btnYaz.onclick=Yazdir;
+
+
+let link = document.querySelector("div>ul>a")
+
+let link = document.querySelector("div>ul>a")
+
